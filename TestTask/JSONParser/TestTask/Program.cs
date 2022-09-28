f8a819652c05385134225947de3e941c74977fe9
Nikita-Rasshivalov <71098405+Nikita-Rasshivalov@users.noreply.github.com>
2022-09-28 18:42:56 +0300

    Create TestTask

---
+++ TestTask/JSONParser/TestTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter path to json file");
+                var filePath = Console.ReadLine();
+                var json = CheckFileExisting(filePath);
+                Console.WriteLine("Enter json path");
+                var jsonPath = Console.ReadLine();
+                Console.WriteLine(ParseJson(jsonPath,json));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //Checking if a file exists
+        public static string CheckFileExisting(string filePath)
+        {
+            bool fileExist = File.Exists(filePath);
+            if (fileExist)
+            {
+                var json = File.ReadAllText(filePath);
+                return json;
+            }
+            else
+            {
+                throw new Exception("File does not exist.");
+            }
+        }
+        //Getting information about a json file
+        public static JToken ParseJson(string jsonPath,string json)
+        {
+            try
+            {
+                JToken o = JToken.Parse(json);
+                JToken currentField = o.SelectToken(jsonPath);
+                if (currentField == null) throw new Exception("error");
+                return currentField;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
